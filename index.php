26e7b707bc36f38dfea77b86b07fffb18c260c8f
Vanh <tranvanh0905@gmail.com>
2019-04-06 10:50:22 +0700

    chinh sua index.php

--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 $url = isset($_GET['url']) == true ? $_GET['url'] : "/";
 $baseUrl = "http://localhost/class4.0/";
 require_once "./Controllers/HomeController.php";
+require_once "./Controllers/UserController.php";
 switch($url)
 {
     case "/":
         $ctr = new HomeController();
         echo $ctr->index();
+        break;
+    case "dang-ky-tai-khoan":
+        $ctr = new HomeController();
+        echo $ctr->registerPage();
+        break;
+    case "dang-nhap-tai-khoan":
+        $ctr = new HomeController();
+        echo $ctr->loginPage();
+        break;
+    case "register":
+        $ctr = new UserController();
+        echo $ctr->registerAccount();
         break;
     default:
         echo "404 not found";
