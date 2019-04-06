ecf4ae67474d01487b1f2b82fe5e13147f887845
Vanh <tranvanh0905@gmail.com>
2019-04-06 09:16:50 +0700

    PHPMailer và AUthTrait

---
+++ Helpers/AuthTrait.php
@@ -0,0 +1,17 @@
+<?php
+require './Vendor/PHPMailer/src/Exception.php';
+require './Vendor/PHPMailer/src/PHPMailer.php';
+require './Vendor/PHPMailer/src/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+trait AuthTrait
+{
+    public function checkAuth()
+    {
+        
+    }
+    public function sendMailConfirm()
+    {
+
+    }
+}
