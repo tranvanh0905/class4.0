d0eefca7b14ba7a96f29499fdda59ab0d74ae8d8
huynie007 <huynie007@gmail.com>
2019-04-06 10:42:43 +0700

    giao diện login, register, index

---
+++ Views/Client/include/footer.php
@@ -0,0 +1,5 @@
+<!-- COMMON SCRIPTS -->
+<script src="../../Public/js/jquery-2.2.4.min.js"></script>
+<script src="../../Public/js/common_scripts.js"></script>
+<script src="../../Public/js/main.js"></script>
+<script src="../../Public/assets/validate.js"></script>	
