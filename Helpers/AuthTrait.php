92eb1fb564bb1209f10d3099fa6f8e421321f728
Vanh <tranvanh0905@gmail.com>
2019-04-15 10:06:34 +0700

    fix

--- Helpers/AuthTrait.php
+++ Helpers/AuthTrait.php
@@ -1,9 +1,5 @@
 <?php
-require './Vendor/PHPMailer/src/Exception.php';
-require './Vendor/PHPMailer/src/PHPMailer.php';
-require './Vendor/PHPMailer/src/SMTP.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+
 trait AuthTrait
 {
     public function checkAuth()
