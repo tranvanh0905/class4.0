df64d9eced49b9d10cc38d8e85bc209c33548be1
huynie007 <huynie007@gmail.com>
2019-04-16 12:40:26 +0700

    chỉnh sửa giao diện học bài

--- Views/Client/learning.php
+++ Views/Client/learning.php
@@ -11,11 +11,16 @@
   <title>Khóa học HTML cơ bản</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="<?= $publicFolder?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= $publicFolder ?>/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="<?= $publicFolder?>/css/learning/css/simple-sidebar.css" rel="stylesheet">
+  
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link href="https://vjs.zencdn.net/7.4.1/video-js.css" rel="stylesheet">
+
+  <!-- If you'd like to support IE8 (for Video.js versions prior to v7) -->
+  <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
+  <link href="<?= $publicFolder ?>/css/learning/css/simple-sidebar.css" rel="stylesheet">
 </head>
 
 <body>
@@ -25,7 +30,7 @@
     <!-- Sidebar -->
     <div class="bg-light border-right" id="sidebar-wrapper">
       <div class="sidebar-heading text-center">
-        <img src="<?= $publicFolder?>/img/logo_black.png" width="135px" height="auto" data-retina="true" alt="">
+        <img src="<?= $publicFolder ?>/img/logo_black.png" width="135px" height="auto" data-retina="true" alt="">
       </div>
       <div class="list-group list-group-flush">
         <a href="#" class="list-group-item list-group-item-action bg-light">Bài 1</a>
@@ -51,13 +56,21 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
             <li class="nav-item active">
-            <a class="nav-link btn btn-danger text-white" href="#"><i class="fas fa-sign-out-alt"></i>  Quay về F-LMS <span class="sr-only">(current)</span></a>
+              <a class="nav-link btn btn-danger text-white" href="#"><i class="fas fa-sign-out-alt"></i> Quay về F-LMS <span class="sr-only">(current)</span></a>
             </li>
           </ul>
         </div>
       </nav>
 
-      <div class="container-fluid">
+      <div class="container-fluid content">
+          <video id='my-video' class='video-js vjs-big-play-centered vjs-16-9' controls preload='auto' width='640' height='264' poster='MY_VIDEO_POSTER.jpg' data-setup='{}'>
+            <source src='<?= $publicFolder ?>/video/intro.mp4' type='video/mp4'>
+            <p class='vjs-no-js'>
+              To view this video please enable JavaScript, and consider upgrading to a web browser that
+              <a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
+            </p>
+          </video>
+            
       </div>
     </div>
     <!-- /#page-content-wrapper -->
@@ -66,9 +79,10 @@
   <!-- /#wrapper -->
 
   <!-- Bootstrap core JavaScript -->
-  <script src="<?= $publicFolder?>/vendor/jquery/jquery.min.js"></script>
-  <script src="<?= $publicFolder?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= $publicFolder ?>/vendor/jquery/jquery.min.js"></script>
+  <script src="<?= $publicFolder ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+  <script src='https://vjs.zencdn.net/7.4.1/video.js'></script>
   <!-- Menu Toggle Script -->
   <script>
     $("#menu-toggle").click(function(e) {
